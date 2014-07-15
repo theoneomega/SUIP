ecf58c18c275ec6abd3fc718ce3aa48b69c402f5
Joel I. Melendez <theoneomega@gmail.com>
2014-07-15 11:53:27 -0600

    antes de galeria...todo funcionando

--- app/uploaders/tattoo_uploader.rb
+++ app/uploaders/tattoo_uploader.rb
@@ -13,7 +13,7 @@ class TattooUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "tatuajes/#{I18n.l model.created_at, :format => '%B'}/#{model.person.name.to_s.underscore} #{model.person.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
